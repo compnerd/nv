f4cbb0a81cfd65a7bf7c986f91027e9dd4990e1b
Saleem Abdulrasool <compnerd@compnerd.org>
2025-10-21 13:39:37 -0700

    build: adjust URL for repositories that are dependencies
    The URL for the repoistories don't need the `.git`, remove them to
    simplify `Package.swift`.

--- Package.swift
+++ Package.swift
@@ -14,8 +14,9 @@ let _ =
               .trait(name: "GNU", description: "GNU C Library")
             ],
             dependencies: [
-              .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.0"),
-              .package(url: "https://github.com/compnerd/swift-platform-core.git", branch: "main",
+              .package(url: "https://github.com/apple/swift-argument-parser",
+                       from: "1.6.0"),
+              .package(url: "https://github.com/compnerd/swift-platform-core", branch: "main",
                        traits: [.trait(name: "GNU", condition: .when(traits: ["GNU"]))]),
             ],
             targets: [
